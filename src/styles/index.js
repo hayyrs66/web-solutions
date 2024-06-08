36615a8b41fcffcc62f3792d93f0dbdc854ecf9a
hayyrs66 <rayrtsx@proton.me>
2024-06-08 16:06:41 -0600

    style: Update text and button styles for better readability and accessibility

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,7 +1,7 @@
 
 export const defaultStyles = {
-    heroParagraph: "text-font-paragraph text-paragraph font-normal leading-snug max-w-2xl text-balance text-center mt-8 mb-8",
-    paragraph: "text-font-paragraph font-normal leading-snug max-w-2xl text-balance",
-    title: "text-white text-[23.8px] font-semibold tracking-tight",
-    subtitleBtn: "text-[#C9CCD2] text-[13.5px] font-medium bg-gray rounded-xl px-3 py-1"
+    heroParagraph: "text-font-paragraph text-black/60 font-normal leading-snug max-w-2xl text-balance text-center mt-8 mb-8",
+    paragraph: "text-font-paragraph font-normal leading-snug text-black/60 max-w-2xl text-balance",
+    title: "text-black text-[23.8px] font-semibold tracking-tight",
+    subtitleBtn: "text-black text-[13.5px] font-medium bg-[#BDFADC] rounded-xl px-3 py-1"
 }
