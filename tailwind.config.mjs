e659ecb00a24a6e763b591c16583eed159e8ef97
hayyrs66 <rayrtsx@proton.me>
2024-06-08 20:09:54 -0600

    refactor: Add pulse animation to tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,7 +14,17 @@ export default {
 
 			fontSize: {
 				'font-paragraph': '0.963rem',
-			}
+			},
+
+			animation: {
+				pulse: 'pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+			},
+			keyframes: {
+				pulse: {
+					'0%, 100%': { opacity: 1 },
+					'50%': { opacity: 0.2 },
+				},
+			},
 
 		},
 	},
