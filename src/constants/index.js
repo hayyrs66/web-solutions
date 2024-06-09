84011558d54747caacffe7aa907460a589a6b53e
hayyrs66 <rayrtsx@proton.me>
2024-06-08 19:01:32 -0600

    refactor: Add howWeWork constant to index.js

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+
+export const howWeWork = [{
+    title: "Recibimos la cotización.",
+    paragraph: "Comenzamos recibiendo tu solicitud de cotización, donde detallas tus necesidades y objetivos. Analizamos la información proporcionada para entender mejor tus requerimientos y ofrecerte una propuesta personalizada que se ajuste a tus expectativas",
+}, {
+    title: "Planificación y Diseño",
+    paragraph: "Después de recibir tu cotización, pasamos a la fase de planificación y diseño. Colaboramos contigo para definir la estructura y el estilo del sitio web, asegurándonos de que refleje la identidad de tu marca y cumpla con tus objetivos.",
+}, {
+    title: "Desarrollo y Programación",
+    paragraph: "Una vez aprobado el diseño, empezamos el desarrollo de tu sitio web. Utilizamos las últimas tecnologías y prácticas de programación para crear un sitio funcional, rápido y seguro",
+}, {
+    title: "Pruebas y Revisión",
+    paragraph: "Realizamos pruebas exhaustivas para asegurar que el sitio web funcione perfectamente en todos los dispositivos y navegadores. Revisamos cada detalle y solucionamos cualquier problema antes del lanzamiento.",
+}, {
+    title: "Lanzamiento y Capacitación",
+    paragraph: "Después de las pruebas y revisiones finales, lanzamos tu sitio web. Además, te proporcionamos capacitación para que puedas gestionar y actualizar el contenido de tu sitio de manera eficiente.",
+}, {
+    title: "Mantenimiento y Soporte",
+    paragraph: "Nuestro trabajo no termina con el lanzamiento. Ofrecemos mantenimiento y soporte continuos para garantizar que tu sitio web se mantenga actualizado y funcione sin problemas.",
+}]
