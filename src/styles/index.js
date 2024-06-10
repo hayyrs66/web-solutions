07e5ec3e922f78f57e71ecd80ae2939ac795b5aa
hayyrs66 <rayrtsx@proton.me>
2024-06-10 10:53:12 -0600

    refactor: Update global styles in Layout.astro
    Improve overall layout and styling of the website by updating global styles in Layout.astro. This includes setting margins and paddings to 0, using border-box for box-sizing, setting a minimum height for the body, and applying consistent font styles to input, button, textarea, and select elements. Additionally, adjust the text-wrap property for headings and paragraphs, and modify the height and max-width properties for images, videos, and SVGs. Enhance user experience with a more consistent and visually appealing design.

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,7 +1,7 @@
 
 export const defaultStyles = {
-    heroParagraph: "text-font-paragraph text-white/60 font-normal leading-snug max-w-2xl text-balance text-center mt-8 mb-8",
-    paragraph: "text-font-paragraph font-normal leading-snug text-white/60 max-w-2xl text-balance",
+    heroParagraph: "text-font-paragraph text-white/60 font-normal leading-snug max-w-2xl  text-center mt-8 mb-8",
+    paragraph: "text-font-paragraph font-normal leading-snug text-white/60 max-w-2xl  ",
     title: "text-white text-[23.8px] font-semibold tracking-tight",
     subtitleBtn: "text-primary text-[13.5px] font-medium bg-primary/20 rounded-xl px-3 py-1"
 }
